e5f5d131357ee36f0818990c88142ccd6848724d
antoineqian <lieuxincertains@gmail.com>
2022-05-30 15:40:05 +0200

    Refactored peak filter coefficient

--- Source/PluginProcessor.cpp
+++ Source/PluginProcessor.cpp
@@ -199,12 +199,16 @@ void SimpleEQAudioProcessor::setStateInformation (const void* data, int sizeInBy
     }
 }
 
+Coefficients makePeakFilter(const ChainSettings& chainSettings,double sampleRate){
+    return      juce::dsp::IIR::Coefficients<float>::makePeakFilter(sampleRate,
+                                                        chainSettings.peakFreq,
+                                                        chainSettings.peakQuality,
+                                                        juce::Decibels::decibelsToGain(chainSettings.peakGainInDecibels));
+}
+
 void SimpleEQAudioProcessor::updatePeakFilter(const ChainSettings &chainSettings){
     
-    auto peakCoefficients = juce::dsp::IIR::Coefficients<float>::makePeakFilter(getSampleRate(),
-                                                                                chainSettings.peakFreq,
-                                                                                chainSettings.peakQuality,
-                                                                                juce::Decibels::decibelsToGain(chainSettings.peakGainInDecibels));
+    auto peakCoefficients = makePeakFilter(chainSettings, getSampleRate());
     
     updateCoefficients(leftChain.get<ChainPositions::Peak>().coefficients, peakCoefficients);
     updateCoefficients(rightChain.get<ChainPositions::Peak>().coefficients, peakCoefficients);
@@ -212,7 +216,7 @@ void SimpleEQAudioProcessor::updatePeakFilter(const ChainSettings &chainSettings
     
 };
 
-void SimpleEQAudioProcessor::updateCoefficients(Coefficients &old, const Coefficients &replacements){
+void updateCoefficients(Coefficients &old, const Coefficients &replacements){
     *old = *replacements;
 }
 
