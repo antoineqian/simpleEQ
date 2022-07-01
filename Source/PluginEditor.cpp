be1b72160ee53fd5db58883fc8cfd8d2584124a1
antoineqian <lieuxincertains@gmail.com>
2022-07-01 14:55:09 +0200

    Add gain labels to grid

--- Source/PluginEditor.cpp
+++ Source/PluginEditor.cpp
@@ -245,9 +245,9 @@ void ResponseCurveComponent::resized(){
     Graphics g (background);
     
     Array<float> freqs{
-        20, 30, 40, 50, 100,
-        200, 300, 400, 500, 1000,
-        2000, 3000, 4000,5000, 10000,
+        20, /* 30, 40, */ 50, 100,
+        200, /* 300, 400, */ 500, 1000,
+        2000, /* 3000, 4000,*/ 5000, 10000,
         20000
     };
     
@@ -276,6 +276,50 @@ void ResponseCurveComponent::resized(){
         auto y = jmap(gdb, -24.f, 24.f, float(bottom), float(top));
         g.setColour(gdb == 0 ? Colour(0u,172u, 1u) : Colours::darkgrey);
         g.drawHorizontalLine(y, left, right);
+    }
+    g.setColour(Colours::lightgrey);
+    const int fontHeight = 10;
+    g.setFont(fontHeight);
+    for (int i =0; i< freqs.size(); ++i){
+        auto f = freqs[i];
+        auto x = xs[i];
+        
+        bool addK = false;
+        if (f > 999.f){
+            f/=1000.f;
+            addK = true;
+        }
+        juce::String str(f, (addK ? 2: 0));
+        
+        if (addK){
+            str <<  "k";
+        }
+        str << "Hz";
+        auto textWidth = g.getCurrentFont().getStringWidth(str);
+        Rectangle<int> r;
+        r.setSize(textWidth, fontHeight);
+        r.setCentre(x, 0);
+        r.setY(1);
+        g.drawFittedText(str, r, juce::Justification::centred, 1);
+        
+    }
+    
+    for (auto gdb: gain){
+        auto y = jmap(gdb, -24.f, 24.f, float(bottom), float(top));
+        juce::String str;
+        if (gdb > 0 ){
+            str << "+";
+        }
+        str << gdb;
+        auto textWidth = g.getCurrentFont().getStringWidth(str);
+        Rectangle<int> r;
+        r.setSize(textWidth, fontHeight);
+        
+        r.setX(getWidth() - textWidth);
+        r.setCentre(r.getCentreX(), y);
+        g.setColour(gdb == 0 ? Colour(0u,172u, 1u) : Colours::lightgrey);
+        g.drawFittedText(str, r, juce::Justification::centred, 1);
+
     }
 }
 
